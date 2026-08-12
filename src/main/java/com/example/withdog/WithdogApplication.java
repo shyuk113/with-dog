b550f303248b9748b801e2cbb358579913eae452
이성혁 <danial2569@gmail.com>
2026-08-12 14:43:05 +0900

    Init project

---
+++ src/main/java/com/example/withdog/WithdogApplication.java
@@ -0,0 +1,13 @@
+package com.example.withdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithdogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithdogApplication.class, args);
+	}
+
+}
