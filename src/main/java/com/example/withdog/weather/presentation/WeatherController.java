1326eb5abd5c2f518cb7d0053e3256f0b07ca868
이성혁 <danial2569@gmail.com>
2026-08-17 01:22:07 +0900

    chore(weather): 오타 수정

--- src/main/java/com/example/withdog/weather/presentation/WeatherController.java
+++ src/main/java/com/example/withdog/weather/presentation/WeatherController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/weahter")
+@RequestMapping("/api/weather")
 @RequiredArgsConstructor
 public class WeatherController {
 
