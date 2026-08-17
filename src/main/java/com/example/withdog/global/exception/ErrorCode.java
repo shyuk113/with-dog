b1c420e970879ce22399f9dfa7fc29cf6779c366
이성혁 <danial2569@gmail.com>
2026-08-17 23:58:38 +0900

    feat(walk): 산책 시작/종료 및 히스토리 조회 API 구현

--- src/main/java/com/example/withdog/global/exception/ErrorCode.java
+++ src/main/java/com/example/withdog/global/exception/ErrorCode.java
@@ -30,7 +30,10 @@ public enum ErrorCode {
     COMMENT_FORBIDDEN(HttpStatus.FORBIDDEN, "본인이 작성한 댓글만 수정/삭제할 수 있습니다."),
 
     // AI
-    AI_SERVER_ERROR(HttpStatus.SERVICE_UNAVAILABLE, "추천 서버 응답에 실패했습니다.");
+    AI_SERVER_ERROR(HttpStatus.SERVICE_UNAVAILABLE, "추천 서버 응답에 실패했습니다."),
+
+    //walkHistory
+    WALKHISTORY_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 산책 이력입니다.");
 
     private final HttpStatus status;
     private final String message;
